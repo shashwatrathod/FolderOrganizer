afb9e74383d2dc09911ac14ddc2550c6782a1d15
Shashwat <shashwatrathod22@gmail.com>
2021-02-08 14:55:29 +0530

    added exception handling

--- organiser.py
+++ organiser.py
@@ -5,13 +5,20 @@ parent_dir = str(input("Paste the directory path(with a forward slash '/'): "))
 
 print("PROCESSING.. \n\n")
 
-DOCS_EXT = ['.pdf', '.doc', '.txt', '.docx', '.xls', '.xlsx', '.ppt', '.ods', '.csv', '.pptx', '.odt', '.tex', '.cfg']
-EXE_EXT = ['.apk', '.bat', '.exe', '.msi', '.bin', '.com', '.cmd', '.jar', '.inf', '.osx', '.reg', '.run', '.rgs', '.ttf']
-IMAGES_EXT = ['.jpg', '.jpeg', '.jfif', '.png', '.ai', '.bmp', '.ico', '.ps', '.psd', '.svg', '.tif', '.tiff', '.raw']
-VIDEOS_EXT = ['.mp4', '.mov', '.3gp', '.flv', '.avi', '.mkv', '.mpg', '.mpeg', '.h264', '.wmv']
-AUDIO_EXT = ['.aif', '.cda', '.mid', '.midi', '.mp3', '.mpa', '.ogg', '.wav', '.wma']
-COMPRESSED_EXT = ['.7z', '.zip', '.rar', '.arj', '.pkg', '.tar.gz', '.z', '.rpm']
-PROG_EXT = ['.json', '.py', '.cpp', '.c', '.css', '.cs', '.html', '.js', '.class', '.java', '.h', '.php', '.sh', '.vb', '.circ', '.ipynb', '.md', '.yml', '.xml']
+DOCS_EXT = ['.pdf', '.doc', '.txt', '.docx', '.xls', '.xlsx',
+            '.ppt', '.ods', '.csv', '.pptx', '.odt', '.tex', '.cfg']
+EXE_EXT = ['.apk', '.bat', '.exe', '.msi', '.bin', '.com', '.cmd',
+           '.jar', '.inf', '.osx', '.reg', '.run', '.rgs', '.ttf']
+IMAGES_EXT = ['.jpg', '.jpeg', '.jfif', '.png', '.ai', '.bmp',
+              '.ico', '.ps', '.psd', '.svg', '.tif', '.tiff', '.raw']
+VIDEOS_EXT = ['.mp4', '.mov', '.3gp', '.flv',
+              '.avi', '.mkv', '.mpg', '.mpeg', '.h264', '.wmv']
+AUDIO_EXT = ['.aif', '.cda', '.mid', '.midi',
+             '.mp3', '.mpa', '.ogg', '.wav', '.wma']
+COMPRESSED_EXT = ['.7z', '.zip', '.rar',
+                  '.arj', '.pkg', '.tar.gz', '.z', '.rpm']
+PROG_EXT = ['.json', '.py', '.cpp', '.c', '.css', '.cs', '.html', '.js', '.class',
+            '.java', '.h', '.php', '.sh', '.vb', '.circ', '.ipynb', '.md', '.yml', '.xml']
 
 if not os.path.exists(os.path.join(parent_dir, 'DOCS')):
     os.makedirs(os.path.join(parent_dir, 'DOCS'))
@@ -39,27 +46,35 @@ if not os.path.exists(os.path.join(parent_dir, 'OTHERS')):
 
 for item in os.listdir(parent_dir):
     item_path = os.path.join(parent_dir, item)
-    
-    if(not os.path.isdir(item) and item not in ['DOCS', 'VIDEOS', 'AUD', 'IMAGES', 'COMPRESSED', 'PROG', 'OTHERS', 'EXECUTABLES']):    
+    print(item_path)
+    try:
+        if(not os.path.isdir(item_path) and item not in ['DOCS', 'VIDEOS', 'AUD', 'IMAGES', 'COMPRESSED', 'PROG', 'OTHERS', 'EXECUTABLES']):
 
-        print(item)
+            print(item)
 
-        if(any(ext in item for ext in DOCS_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'DOCS', item))
-        elif(any(ext in item for ext in EXE_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'EXECUTABLES', item))
-        elif(any(ext in item for ext in IMAGES_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'IMAGES', item))
-        elif(any(ext in item for ext in VIDEOS_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'VIDEOS', item))
-        elif(any(ext in item for ext in AUDIO_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'AUD', item))
-        elif(any(ext in item for ext in COMPRESSED_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'COMPRESSED', item))
-        elif(any(ext in item for ext in PROG_EXT)):
-            shutil.move(item_path, os.path.join(parent_dir, 'PROG', item))
-        else:
-            shutil.move(item_path, os.path.join(parent_dir, 'OTHERS', item))
+            if(any(ext in item for ext in DOCS_EXT)):
+                shutil.move(item_path, os.path.join(parent_dir, 'DOCS', item))
+            elif(any(ext in item for ext in EXE_EXT)):
+                shutil.move(item_path, os.path.join(
+                    parent_dir, 'EXECUTABLES', item))
+            elif(any(ext in item for ext in IMAGES_EXT)):
+                shutil.move(item_path, os.path.join(
+                    parent_dir, 'IMAGES', item))
+            elif(any(ext in item for ext in VIDEOS_EXT)):
+                shutil.move(item_path, os.path.join(
+                    parent_dir, 'VIDEOS', item))
+            elif(any(ext in item for ext in AUDIO_EXT)):
+                shutil.move(item_path, os.path.join(parent_dir, 'AUD', item))
+            elif(any(ext in item for ext in COMPRESSED_EXT)):
+                shutil.move(item_path, os.path.join(
+                    parent_dir, 'COMPRESSED', item))
+            elif(any(ext in item for ext in PROG_EXT)):
+                shutil.move(item_path, os.path.join(parent_dir, 'PROG', item))
+            else:
+                shutil.move(item_path, os.path.join(
+                    parent_dir, 'OTHERS', item))
+    except:
+        print("error occured while processing "+item)
+
 
 print("\n\nDONE..!")
-        
